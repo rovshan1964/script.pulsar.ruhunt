6c83cab99a8ad7608b0696485b3a4e2c6ded5ead
maxkhvorov <maxmang@gmail.com>
2014-12-25 11:15:24 +0300

    Fix maint`s scripts for Kodi

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`/../src
-cp -r ./* $HOME/Library/Application\ Support/XBMC/addons/script.pulsar.ruhunt/
+cp -r ./* $HOME/Library/Application\ Support/Kodi/addons/script.pulsar.ruhunt/
